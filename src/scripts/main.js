030df8a3c1d267ce660a012699222e5bdb68667a
DanielSouza-de <daniel.desouza2007@gmail.com>
2026-03-10 23:58:20 -0300

    adicionando tempo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,28 @@
 const contador = document.getElementById("contador");
 
 if (contador) {
+  const dataDoEvento = new Date("Mar 11, 2026 00:10:00").getTime();
 
-    const dataDoEvento = new Date("Mar 11, 2026 00:00:00").getTime();
+  setInterval(() => {
+    const agora = new Date().getTime();
+    const distancia = dataDoEvento - agora;
 
-    setInterval(() => {
+    const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor(
+      (distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    );
+    const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
 
-        const agora = new Date().getTime();
-        const distancia = dataDoEvento - agora;
+    contador.innerHTML = `${dias}d ${horas}h ${minutos}m ${segundos}s`;
 
-        const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
-        const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
-        const segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+    if (distancia <= 0) {
+      document.body.style.transition = "opacity 1s";
+      document.body.style.opacity = "0";
 
-        contador.innerHTML =
-        `${dias}d ${horas}h ${minutos}m ${segundos}s`;
-
-        if (distancia <= 0) {
-
-            document.body.style.transition = "opacity 1s";
-            document.body.style.opacity = "0";
-
-            setTimeout(() => {
-                window.location.href = "surpresa.html";
-            }, 1000);
-
-        }
-
-    }, 1000);
-
+      setTimeout(() => {
+        window.location.href = "surpresa.html";
+      }, 1000);
+    }
+  }, 1000);
 }
